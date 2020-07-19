07b03efdb7b0adc1c41f29315a7170f4e9cdc779
Adin Horovitz <adin@tapclicks.com>
2020-07-19 14:46:49 -0700

    implemented new task functionality. Persistence of updated lists

--- app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
@@ -1,7 +1,13 @@
 package com.raywenderlich.listmaker
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.ActionMenuView
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import kotlinx.android.synthetic.main.activity_list_detail.*
@@ -19,5 +25,44 @@ class ListDetailActivity : AppCompatActivity() {
 
         listItemsRecyclerView.adapter = ListItemsRecyclerViewAdapter(list)
         listItemsRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        addTaskButton.setOnClickListener {
+            showCreateTaskDialog()
+        }
+    }
+
+    private fun showCreateTaskDialog() {
+        val taskEditText = EditText(this)
+        taskEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.task_to_add)
+            .setView(taskEditText)
+            .setPositiveButton(R.string.add_task) { dialog, _ ->
+                val task = taskEditText.text.toString()
+                list.tasks.add(task)
+
+                val recyclerAdapter = listItemsRecyclerView.adapter as ListItemsRecyclerViewAdapter
+                recyclerAdapter.notifyItemInserted(list.tasks.size - 1)
+
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    /**
+     * Runs whenever back button is pressed on activity
+     * --> Bundles updated list and returns via Intent
+     */
+    override fun onBackPressed() {
+        val bundle = Bundle()
+        bundle.putParcelable(MainActivity.INTENT_LIST_KEY, list)
+
+        val intent = Intent()
+        intent.putExtras(bundle)
+        setResult(Activity.RESULT_OK, intent)
+
+        super.onBackPressed()
     }
 }
